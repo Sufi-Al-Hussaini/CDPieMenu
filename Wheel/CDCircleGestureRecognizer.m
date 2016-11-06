8585f2b6cb668c916f1c3454f963afaf77807eec
Shoaib Ahmed <sufialhussaini@gmail.com>
2016-11-06 16:32:14 +0400

    Fixed move to position on tap

--- Wheel/CDCircleGestureRecognizer.m
+++ Wheel/CDCircleGestureRecognizer.m
@@ -46,138 +46,10 @@
    } else {
       [self setState:UIGestureRecognizerStateChanged];
    }
-
-   // We can look at any touch object since we know we 
-   // have only 1. If there were more than 1 then 
-   // touchesBegan:withEvent: would have failed the recognizer.
-   UITouch *touch = [touches anyObject];
-
-   // To rotate with one finger, we simulate a second finger.
-   // The second figure is on the opposite side of the virtual
-   // circle that represents the rotation gesture.
-
-    CDCircle *view = (CDCircle *) [self view];
-    if (view.rotate == NO) {
-        return;
-    }
-    
-   CGPoint center = CGPointMake(CGRectGetMidX([view bounds]), CGRectGetMidY([view bounds]));
-   CGPoint currentTouchPoint = [touch locationInView:view];
-   CGPoint previousTouchPoint = [touch previousLocationInView:view];
-    previousTouchDate = [NSDate date];
-    CGFloat angleInRadians = atan2f(currentTouchPoint.y - center.y, currentTouchPoint.x - center.x) - atan2f(previousTouchPoint.y - center.y, previousTouchPoint.x - center.x);
-   [self setRotation:angleInRadians];
-    currentTransformAngle = atan2f(view.transform.b, view.transform.a);
-    
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    CDCircle *view = (CDCircle *) [self view];
-    if (view.rotate == NO) {
-        UITouch *touch = [touches anyObject];
-        
-        for (CDCircleThumb *thumb in view.thumbs) {
-            
-            CGPoint touchPoint = [touch locationInView:thumb];
-            if (CGPathContainsPoint(thumb.arc.CGPath, NULL, touchPoint, NULL)) {
-                [view.delegate circle:view didMoveToSegment:thumb.tag thumb:thumb];
-            }
-        }
-        return;
-    }
-
-    // Perform final check to make sure a tap was not misinterpreted.
-   if ([self state] == UIGestureRecognizerStateChanged) {
-    
-       
-       CDCircle *view = (CDCircle *) [self view];
-       CGFloat flipintime = 0;
-       CGFloat angle = 0;
-       if (view.inertiaeffect == YES) {
-           CGFloat angleInRadians = atan2f(view.transform.b, view.transform.a) - currentTransformAngle;
-           double time = [[NSDate date] timeIntervalSinceDate:previousTouchDate];
-           double velocity = angleInRadians/time;
-           CGFloat a = deceleration_multiplier;
-           
-            flipintime = fabs(velocity)/a; 
-           
-           
-           
-            angle = (velocity*flipintime)-(a*flipintime*flipintime/2);
-           
-           if (angle>M_PI/2 || (angle<0 && angle<-1*M_PI/2)) {
-               if (angle<0) {
-                   angle =-1 * M_PI/2.1f;
-               }    
-               else { angle = M_PI/2.1f; }
-               
-               flipintime = 1/(-1*(a/2*velocity/angle));
-           }
-
-       }
-       
-       
-              
-       [UIView animateWithDuration:flipintime delay:0.f options:UIViewAnimationOptionCurveEaseOut animations:^{
-           [view setTransform:CGAffineTransformRotate(view.transform,angle)];
-
-       } completion:^(BOOL finished) {
-           for (CDCircleThumb *thumb in view.thumbs) {
-               
-               
-               CGPoint point = [thumb convertPoint:thumb.centerPoint toView:nil];
-               CDCircleThumb *shadow = view.overlayView.overlayThumb;
-               CGRect shadowRect = [shadow.superview convertRect:shadow.frame toView:nil];
-               
-               if (CGRectContainsPoint(shadowRect, point) == YES) {
-                   CGPoint pointInShadowRect = [thumb convertPoint:thumb.centerPoint toView:shadow];
-                   if (CGPathContainsPoint(shadow.arc.CGPath, NULL, pointInShadowRect, NULL)) {
-                       CGAffineTransform current = view.transform;
-                   
-                       
-                    CGFloat deltaAngle= - degreesToRadians(180) + atan2(view.transform.a, view.transform.b) + atan2(thumb.transform.a, thumb.transform.b);
-
-                       
-                        [UIView animateWithDuration:0.2f animations:^{
-                       [view setTransform:CGAffineTransformRotate(current, deltaAngle)];
-                        }];
-                       
-                       
-                       
-                       
-                       
-
-                       SystemSoundID soundID;
-                       NSString *filePath = [[NSBundle mainBundle] pathForResource:@"iPod Click" ofType:@"aiff"];
-                       
-                       NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
-                       AudioServicesCreateSystemSoundID((__bridge CFURLRef)fileUrl, &soundID);
-                       AudioServicesPlaySystemSound(soundID);
-                       
-                       [currentThumb.iconView setIsSelected:NO];
-                       [thumb.iconView setIsSelected:YES];
-                       self.currentThumb = thumb;
-                       //Delegate method
-                       [view.delegate circle:view didMoveToSegment:thumb.tag thumb:thumb];
-                       self.ended = YES;
-                       break;
-                   }
-                   
-               }            
-           }
-;
-       }];
-
-
-       currentTransformAngle = 0;
-       
-       
-              
-     [self setState:UIGestureRecognizerStateEnded];  
-       
-   } else {
-       
        CDCircle *view = (CDCircle *)[self view];
        UITouch *touch = [touches anyObject];
        
@@ -214,7 +86,6 @@
        }
        
        [self setState:UIGestureRecognizerStateFailed];
-   }
 }
 
 
